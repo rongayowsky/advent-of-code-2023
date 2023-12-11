0abfdd5ccf01f6b4e142eb10e5197d38d6ab9633
Ronald Gayowsky <ron.gayowsky@gmail.com>
2023-12-10 23:48:51 -0500

    Complete Day 1 part 2

--- day1/calibrationDocSolver.hpp
+++ day1/calibrationDocSolver.hpp
@@ -36,68 +36,162 @@ class CalibrationDocParser {
     std::vector<std::string> m_lines;
 };
 
+/*
+now we need to detect spelled numbers as well. can grab handfill of chars in
+buckets of 3, 4, and 5 chunks, optionally reverse the spelling, and check a
+hashmap for the match.
+
+one two three four five six seven eight nine.
+
+early exit condition, only check first letter to see if in list before string
+matching:
+
+starting letters:
+o t f s e n
+
+ending numbers:
+e o r x n t
+*/
+
 class CalibrationDocSolver {
    public:
-    CalibrationDocSolver(std::vector<std::string> &calibrationDocLines)
-        : m_calibrationDocLines(calibrationDocLines) {
+    CalibrationDocSolver(std::vector<std::string> &calibrationDocLines,
+                         bool matchSpelledNumbers = false)
+        : m_calibrationDocLines(calibrationDocLines),
+          M_MATCH_SPELLED_NUMBERS(matchSpelledNumbers) {
         recoverCalibrationValsFromDoc();
     }
 
-    uint16_t getCalibrationSum() {
-        uint16_t sum{0};
-        for (uint8_t &calibrationVal : m_calibrationVals) {
+    int getCalibrationSum() {
+        int sum{0};
+        for (int &calibrationVal : m_calibrationVals) {
             sum += calibrationVal;
         }
         return sum;
     }
 
-    std::vector<uint8_t> getCalibrationVals() { return m_calibrationVals; }
+    std::vector<int> getCalibrationVals() { return m_calibrationVals; }
 
    private:
     std::vector<std::string> m_calibrationDocLines;
-    std::vector<uint8_t> m_calibrationVals{};
-    const int TENS_DIGIT_FACTOR{10};
-    const int ONES_DIGIT_FACTOR{1};
-    const char FIRST_ASCII_INT{'0'};
+    std::vector<int> m_calibrationVals{};
+    const int M_TENS_DIGIT_FACTOR{10};
+    const int M_ONES_DIGIT_FACTOR{1};
+    const char M_FIRST_ASCII_INT{'0'};
+    bool M_MATCH_SPELLED_NUMBERS;
+    std::map<std::string, int> SPELLED_NUMBERS = {
+        {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
+        {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
+    };
+
+    // used for reduction of rudundant checks
+    const std::string STARTING_CHARS = "otfsen";
+    const std::string ENDING_CHARS = "eorxnt";
+    const int MAX_WORD_LENGTH = 5;
 
     void recoverCalibrationValsFromDoc() {
+        int i = 0;
         for (std::string &line : m_calibrationDocLines) {
-            std::pair<uint8_t, uint8_t> vals = getCalibrationValsFromLine(line);
-            uint8_t val = vals.first * TENS_DIGIT_FACTOR +
-                          vals.second * ONES_DIGIT_FACTOR;
+            std::pair<int, int> vals = getCalibrationValsFromLine(line);
+            int val = vals.first * M_TENS_DIGIT_FACTOR +
+                      vals.second * M_ONES_DIGIT_FACTOR;
             m_calibrationVals.push_back(val);
         }
     }
 
-    std::pair<uint8_t, uint8_t> getCalibrationValsFromLine(std::string &line) {
-        uint8_t firstDigit{0}, secondDigit{0};
+    std::pair<int, int> getCalibrationValsFromLine(std::string &line) {
+        int firstDigit{0}, secondDigit{0};
         bool setFirst{false}, setSecond{false};
         for (int i{0}; i <= line.size(); ++i) {
             const char c1 = line[i];
             if (!setFirst && isInt(c1)) {
                 firstDigit = toInt(c1);
                 setFirst = true;
+            } else if (M_MATCH_SPELLED_NUMBERS && !setFirst) {
+                int value = matchSpelledNumberLeftToRight(line, i);
+                if (value >= 0) {
+                    firstDigit = value;
+                    setFirst = true;
+                }
             }
 
             const char c2 = line[line.size() - i];
             if (!setSecond && isInt(c2)) {
                 secondDigit = toInt(c2);
                 setSecond = true;
+            } else if (M_MATCH_SPELLED_NUMBERS && !setSecond) {
+                int value =
+                    matchSpelledNumberRightToLeft(line, line.size() - i);
+                if (value >= 0) {
+                    secondDigit = value;
+                    setSecond = true;
+                }
             }
 
             if (setFirst && setSecond) {
                 break;
             }
         }
-        return std::pair<uint8_t, uint8_t>(firstDigit, secondDigit);
+        return std::pair<int, int>(firstDigit, secondDigit);
+    }
+
+    int matchSpelledNumberLeftToRight(std::string &line, int index) {
+        // Early exit if char is not a beginning character of 1-9
+        if (STARTING_CHARS.find(line[index]) == std::string::npos) {
+            return -1;
+        }
+        return matchSpelledNumber(line.substr(index, MAX_WORD_LENGTH));
+    }
+
+    int matchSpelledNumberRightToLeft(std::string &line, int index) {
+        // Early exit if char is not an ending character of 1-9
+        // NOTE: there be a bug that is trigger when this optimization is used.
+        // See test SpelledNumberTrickyExampleValidation.
+        // if (ENDING_CHARS.find(line[index]) == std::string::npos) {
+        //     return -1;
+        // }
+
+        int saneIndex{index - MAX_WORD_LENGTH + 1};
+        int lengthReduction{0};
+        if (saneIndex < 0) {
+            saneIndex = 0;
+            lengthReduction = index - MAX_WORD_LENGTH + 1;
+        }
+        return matchSpelledNumber(
+            line.substr(saneIndex, MAX_WORD_LENGTH + 1 + lengthReduction),
+            false);
+    }
+
+    int matchSpelledNumber(const std::string subString,
+                           const bool isLeftToRight = true) {
+        int value{-1};
+
+        // Try to match character to spelled number
+        for (int length{3}; length <= 5; ++length) {
+            if (isLeftToRight && SPELLED_NUMBERS.find(subString.substr(
+                                     0, length)) != SPELLED_NUMBERS.end()) {
+                value = SPELLED_NUMBERS.at(subString.substr(0, length));
+                break;
+            } else if (!isLeftToRight &&
+                       SPELLED_NUMBERS.find(subString.substr(
+                           MAX_WORD_LENGTH - length, length)) !=
+                           SPELLED_NUMBERS.end()) {
+                value = SPELLED_NUMBERS.at(
+                    subString.substr(MAX_WORD_LENGTH - length, length));
+                break;
+            }
+        }
+        return value;
     }
 
     bool isInt(char c) {
-        int val = c - FIRST_ASCII_INT;
+        int val = c - M_FIRST_ASCII_INT;
         return (val >= 0 && val <= 9);
     }
 
-    uint8_t toInt(char c) { return c - FIRST_ASCII_INT; }
+    // Look up ASCII character mapping to understand how this subtraction leads
+    // to the numerical value: https://www.asciitable.com/
+    int toInt(char c) { return c - M_FIRST_ASCII_INT; }
 };
 
 #endif   // CALIBRATION_DOC_SOLVER_H
--- day1/calibrationDocSolverTests.cpp
+++ day1/calibrationDocSolverTests.cpp
@@ -10,11 +10,27 @@ TEST(CalibrationDocSolverTest, NullValidation) {
     ASSERT_EQ(solver.getCalibrationVals().size(), 0);
 }
 
-TEST(CalibrationDocSolverTest, NoWorkValidation) {
+TEST(CalibrationDocSolverTest, SpelledNumberNullValidation) {
+    std::vector<std::string> calibrationDoc{};
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(), 0);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 0);
+}
+
+TEST(CalibrationDocSolverTest, MinimalWorkValidation) {
     std::vector<std::string> calibrationDoc{};
     calibrationDoc.push_back("12");
 
     CalibrationDocSolver solver{calibrationDoc};
+    ASSERT_EQ(solver.getCalibrationSum(), 12);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 1);
+}
+
+TEST(CalibrationDocSolverTest, SpelledNumberMinimalWorkValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("onetwo");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
     ASSERT_EQ(solver.getCalibrationSum(), 12);
     ASSERT_EQ(solver.getCalibrationVals().size(), 1);
 }
@@ -28,7 +44,25 @@ TEST(CalibrationDocSolverTest, SingleNumberValidation) {
     ASSERT_EQ(solver.getCalibrationVals().size(), 1);
 }
 
-TEST(CalibrationDocSolverTest, exampleValidation) {
+TEST(CalibrationDocSolverTest, SpelledNumberSingleValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("one");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(), 11);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 1);
+}
+
+TEST(CalibrationDocSolverTest, SpelledNumberOneLineValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("asdfonetwothree4five");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(), 15);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 1);
+}
+
+TEST(CalibrationDocSolverTest, ExampleValidation) {
     std::vector<std::string> calibrationDoc{};
     calibrationDoc.push_back("1abc2");
     calibrationDoc.push_back("pqr3stu8vwx");
@@ -40,13 +74,69 @@ TEST(CalibrationDocSolverTest, exampleValidation) {
     ASSERT_EQ(solver.getCalibrationVals().size(), 4);
 }
 
-TEST(CalibrationDocSolverTest, fullSumOfCalibrationValues) {
+TEST(CalibrationDocSolverTest, SpelledNumberExampleValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("two1nine");
+    calibrationDoc.push_back("eightwothree");
+    calibrationDoc.push_back("abcone2threexyz");
+    calibrationDoc.push_back("xtwone3four");
+    calibrationDoc.push_back("4nineeightseven2");
+    calibrationDoc.push_back("zoneight234");
+    calibrationDoc.push_back("7pqrstsixteen");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(), 281);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 7);
+}
+
+TEST(CalibrationDocSolverTest, SpelledNumberTrickyExampleValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("eighthree");
+    calibrationDoc.push_back("sevenine");
+    calibrationDoc.push_back("abc2x3oneight");
+    calibrationDoc.push_back("7sixjhhzhgh");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(), 83 + 79 + 28 + 76);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 4);
+}
+
+TEST(CalibrationDocSolverTest, SpelledNumberMapValidation) {
+    std::vector<std::string> calibrationDoc{};
+    calibrationDoc.push_back("one");
+    calibrationDoc.push_back("two");
+    calibrationDoc.push_back("three");
+    calibrationDoc.push_back("four");
+    calibrationDoc.push_back("five");
+    calibrationDoc.push_back("six");
+    calibrationDoc.push_back("seven");
+    calibrationDoc.push_back("eight");
+    calibrationDoc.push_back("nine");
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    ASSERT_EQ(solver.getCalibrationSum(),
+              11 + 22 + 33 + 44 + 55 + 66 + 77 + 88 + 99);
+    ASSERT_EQ(solver.getCalibrationVals().size(), 9);
+}
+
+TEST(CalibrationDocSolverTest, FullSumOfCalibrationValues) {
     std::string filepath = "calibrationDoc.txt";
     CalibrationDocParser parser{filepath};
     std::vector<std::string> calibrationDoc{parser.getCalibrationLines()};
 
     CalibrationDocSolver solver{calibrationDoc};
     std::cout << "sum = " << solver.getCalibrationSum() << std::endl;
+    ASSERT_EQ(solver.getCalibrationSum(), 56042);   // verified answer
+}
+
+TEST(CalibrationDocSolverTest, SpelledNumberFullSumOfCalibrationValues) {
+    std::string filepath = "calibrationDoc.txt";
+    CalibrationDocParser parser{filepath};
+    std::vector<std::string> calibrationDoc{parser.getCalibrationLines()};
+
+    CalibrationDocSolver solver{calibrationDoc, true};
+    std::cout << "sum = " << solver.getCalibrationSum() << std::endl;
+    ASSERT_EQ(solver.getCalibrationSum(), 55358);   // verified answer
 }
 
 int
